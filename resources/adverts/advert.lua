1b306951eb5be92c0e1ccc6af93518621c376ad2
cody196 <cody.wherry@gmail.com>
2016-07-05 00:48:53 -0400

    Update advert.lua

--- resources/adverts/advert.lua
+++ resources/adverts/advert.lua
@@ -1,5 +1,14 @@
-local chatMessageEntered ()
+local ChatMessageEntered (console, message)
+
+message == "No Deathmatching"
+message == "No Teleproting"
+message == "Need help? Ask an admin or the owner Cody196 / cawhe"
 
 
-function sendMessage (chatBox, Global) 
-  message == "No Deathmatching"
+function sendMessage (chatBox, message,  Global) 
+  RegisterServerEvent(ChatMessageEntered, message)
+    wait(500)
+  if wait hasReached then repeat
+    sendMessage(Message, math.random)
+  else cancelEvent()
+end
