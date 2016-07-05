70f34c00977ddbd2c07057bc341aae46dad4f087
cody196 <cody.wherry@gmail.com>
2016-07-05 01:42:12 -0400

    Update arrest_client.lua

--- resources/arrest/arrest_client.lua
+++ resources/arrest/arrest_client.lua
@@ -1,22 +1,23 @@
 function checkWantedLevel(thePlayer, playerName, wantedLevel)
-	GET_PLAYER_WANTED_LEVEL(thePlayer, wantedLevel)
+	TriggerClientEvent(thePlayer, checkWantedLevel)
+	GetWantedLevel(thePlayer, wantedLevel)
 	if thePlayer(hasWantedLevel)
-	then displayName
-	else GET_TIME_SINCE_LAST_ARREST(time)
+	then print( ..name.. "has" ..WantedLevel)
+	else cancelEvent()
 end
 
-function tazed(thePlayer, weapon, frozen)
-	weapon("stun_gun")
-	if playerHit
-	then freezePlayer(x, y, z)
-	vector3.playerPosition.getModel.getPosition
+function tazed (thePlayer, weapon)
+	weapon = "stun_gun"
+	if tazed then
+	FreezePlayer(thePlayer, GetPosition)
+	local position = (thePlayer):GetPosition(x, y, z)
 
-function arrest(command, thePlayer)
-	IS_PLAYER_BEING_ARRESTED(true)
-	PLAY_ENTITY_ANIM("random@arrests@busted")
-	IS_PED_CUFFED(true)
-	PLAY_ENTITY_ANIM("get_up@cuffed")
-	PLAYER.CLEAR_PLAYER_WANTED_LEVEL(thePlayer)
-	SET_PLAYER_WANTED_LEVEL_now(0)
-	addEventHandler("arrest", arrest, thePlayer)
+function arrest(commandEntered, thePlayer)
+	commandEntered("/arrest" ..PlayerName)
+	if IsBeingArrested = true then
+	PlayAnim("random@arrests@busted")
+	elseif IS_PED_CUFFED = true then
+	PlayAnim("get_up@cuffed")
+	ClearWantedLevel(thePlayer)
+	addEventHandler(arrest, thePlayer)
 end
