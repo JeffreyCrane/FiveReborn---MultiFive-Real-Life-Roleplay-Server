27dbfb9907f4e7289a9e670f94535d5b45804afa
cody196 <cody.wherry@gmail.com>
2016-07-12 03:43:30 -0400

    Update __resource.lua

--- resources/localchat/__resource.lua
+++ resources/localchat/__resource.lua
@@ -1,1 +1,15 @@
-chat "localchat"
+description 'localchat'
+
+ui_page 'html/chat.html'
+
+client_script 'localchat_client.lua'
+server_script 'localchat_server.lua'
+
+export 'printChatLine'
+
+files {
+    'html/chat.html',
+    'html/chat.css',
+    'html/chat.js',
+    'html/jquery.faketextbox.js'
+}
