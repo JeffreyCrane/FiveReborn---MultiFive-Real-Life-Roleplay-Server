7f3c573dd0926b9597786e3da31bae5c356a8f40
cody196 <cody.wherry@gmail.com>
2016-07-13 18:24:09 -0400

    Update kick.lua

--- resources/admin/kick.lua
+++ resources/admin/kick.lua
@@ -1,20 +1,25 @@
-function TempBanPlayer (player, command, name, reason)
-	command ("/kick", player, reason)
-  if command then
-  if ( hasObjectPermissionTo ( player, "function.TempBanPlayer" ) ) then
-		--Get player element from the name
-		local kickedPlayer = GetPlayerName ( name )
-		
-		TempBanPlayer ( kickedPlayer, player, reason )
-	  	print ( "Kick:" .. name .. " has been kicked for " .. reason .. )
-	  else
-	  	print ( "You do not have permission to kick players" )
-	  end
-	end
-end
-AddEventHandler ( "kick", TempBanPlayer )
 
 
-function NetworkSessionKickPlayer(player)
-	RegisterServerEvent ("function.TempBanPlayer")
+
+
+
+--function dropPlayer (player, command, name, reason)
+--	command ("/kick", player, reason)
+--  if command then
+--  if ( hasObjectPermissionTo ( player, "function.TempBanPlayer" ) ) then
+--		--Get player element from the name
+--		local kickedPlayer = GetPlayerName ( name )
+--		
+--		dropPlayer ( kickedPlayer, player, reason )
+--	  	print ( "Kick:" .. name .. " has been kicked for " .. reason .. )
+--	  else
+--	  	print ( "You do not have permission to kick players" )
+--	  end
+--	end
+--end
+--AddEventHandler ( "kick", dropPlayer )
+--
+--
+--function NetworkSessionKickPlayer(player)
+--	RegisterServerEvent ("function.dropPlayer")
 
