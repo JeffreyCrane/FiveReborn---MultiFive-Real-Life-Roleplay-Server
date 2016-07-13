bca65ac667f7959ae3bc7abff963f308d7c8fb68
cody196 <cody.wherry@gmail.com>
2016-07-12 23:17:17 -0400

    Update arrest_server.lua

--- resources/arrest/arrest_server.lua
+++ resources/arrest/arrest_server.lua
@@ -1,8 +1,8 @@
 local players = GetElementType ( "player" ) -- get a table of all the players in the server
-for theKey,thePlayer in ipairs(players) do -- use a generic for loop to step through each player
-   local level = GetWantedLevel ( thePlayer ) -- get the wanted level of the player
+for theKey,player in ipairs(players) do -- use a generic for loop to step through each player
+   local level = GetWantedLevel ( player ) -- get the wanted level of the player
    if ( level > (WantedLevel) ) then -- if the player has any stars, announce it in the chat:
-      print ( GetPlayerName ( thePlayer ) .. " has a wanted level of " .. level .. "  stars!" )
+      print ( GetPlayerName ( name ) .. " has a wanted level of " .. level .. "  stars!" )
    end 
 end
 
