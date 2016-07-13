49572c83b44f70fa9ff8af45a1614ba466b609a3
cody196 <cody.wherry@gmail.com>
2016-07-12 23:38:35 -0400

    Update advert.lua

--- resources/adverts/advert.lua
+++ resources/adverts/advert.lua
@@ -1,14 +1,20 @@
+--Advertisements by Cody196--
+
+RegisterServerEvent (onResourceStart, chatMessage)
+
 local ChatMessageEntered (console, message)
 
+-- the messages, to add more just type print ("the message") on the next line, leave two spaces
 print ("AD: No Deathmatching")
 print ("AD: No Teleporting")
 print ("AD: Need help? Ask an admin or the owner Cody196 / cawhe")
 
 
-function sendMessage (message,  all) 
+-- sends the message to chat
+function chatMessage (message,  all) 
   RegisterServerEvent(ChatMessageEntered, message)
-    wait(500)
+    wait(120000)   -- wait time in milliseconds
     if wait then
-    repeat sendMessage and random.print
-  else cancelEvent()
+    repeat chatMessage    -- repeats from beginning over and over in a loop
+  else cancelEvent()    -- cancels event only if there is an error or mishap
 end
