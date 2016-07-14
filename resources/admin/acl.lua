a028aeb176fe5f15a78f9f821b7762661fd77822
cody196 <cody.wherry@gmail.com>
2016-07-13 22:24:12 -0400

    Update acl.lua

--- resources/admin/acl.lua
+++ resources/admin/acl.lua
@@ -1,2 +1,12 @@
-local ACL = (hasPermissionTo, source)
+local ACL = hasPermissionTo(source)
 local playerName = GetPlayerName(source)
+
+ACLList = "acl.txt"
+
+function ACL(source, hasPermissionTo)
+  GetPlayerNameFromList(ACLList)
+    if ( PlayerNameIsInList(source)) then
+    givePermissionTo(source, ACL)
+    else print ("You are not an admin") then
+  CancelEvent()
+end
