51e56bb11dc9344d62d8cab151b9694c602e37f5
Galaxy <GalaxyNL@users.noreply.github.com>
2016-07-15 23:47:54 +0200

    Update arrest_client.lua (#5)
    Fixed the output on CheckWantedLevel.
    
    Also updated the layout :)!

--- resources/arrest/arrest_client.lua
+++ resources/arrest/arrest_client.lua
@@ -1,11 +1,11 @@
---function checkWantedLevel(name, WantedLevel)
---	TriggerClientEvent(name, command, CheckPlayerWantedLevel)
---	if commandEntered == "/wanted "..playerName) then
---	GetPlayerWantedLevel(WantedLevel)
---	if GetPlayerWantedLevel then
---	print( ..name.. "has" ..WantedLevel)
---	else cancelEvent()
---end
+function checkWantedLevel(name, WantedLevel)
+	TriggerClientEvent(name, command, CheckPlayerWantedLevel)
+		if commandEntered == "/wanted "..playerName) then
+			GetPlayerWantedLevel(WantedLevel)
+		if GetPlayerWantedLevel then
+			TriggerEvent('chatMessage', ..name.. " has a wanted level of " ..GetPlayerWantedLevel.. "!") -- This works only when followed up by the default guide: http://wiki.kanersps.pw/#Basic%20Scripting --> See change of chat resource!
+	else cancelEvent()
+end
 
 --function tazed (thePlayer, weapon)
 --	weapon = "stun_gun"
@@ -15,10 +15,10 @@
 
 function arrest(commandEntered, thePlayer)
 	commandEntered("/arrest" ..PlayerName)
-	if IsBeingArrested = true then
-	PlayAnim("random@arrests@busted")
-	elseif IS_PED_CUFFED = true then
-	PlayAnim("get_up@cuffed")
-	ClearWantedLevel(thePlayer)
-	addEventHandler(arrest, thePlayer)
+		if IsBeingArrested = true then
+			PlayAnim("random@arrests@busted")
+		elseif IS_PED_CUFFED = true then
+			PlayAnim("get_up@cuffed")
+			ClearWantedLevel(thePlayer)
+			addEventHandler(arrest, thePlayer)
 end
