0dcce5f0ec0eeffdb7d19581ff5b6a7d10ee9745
Galaxy <GalaxyNL@users.noreply.github.com>
2016-07-17 20:39:25 +0200

    Code could be simplified. (#6)
    SetVehicleEngineHealth instead to supply the command ID of the vehicle.

--- resources/VehicleRepair/repair_client.lua
+++ resources/VehicleRepair/repair_client.lua
@@ -1,16 +1,15 @@
 RegisterNetEvent("SetVehicleFixed")
 AddEventHandler('SetVehicleFixed', function(message)
     Citizen.CreateThread(function() -- Created a new thread
-        Citizen.Wait(50) -- A wait function for 50 miliseconds.
-        local carid = GetHashKey(message) -- Gets the car id from the message received.
-        local playerPed = GetPlayerPed(-1) -- Gets the player that ran the command.
+    Citizen.Wait(50) -- A wait function for 50 miliseconds.
+    local playerPed = GetPlayerPed(-1) -- Gets the player that ran the command.
         if playerPed and playerPed ~= -1 then -- Checks if the player exists
-        end
-        local car = SetVehicleFixed(carid, playerCoords, 0.0, true, false)
+            local car = GetVehiclePedIsIn(playerPed,false)
+            SetVehicleEngineHealth(car, 1000)
+            TriggerEvent('chatMessage', 'VehicleRepair:', {0, 255, 0}, "Vehicle fixed.")
         elseif IsPedOnFoot(GetPlayerPed(-1)) then
-        print ("You are not in a vehicle")
-
-            TriggerEvent('chatMessage', 'SYSTEM', {0, 255, 0}, "Vehicle fixed.")
+            TriggerEvent('chatMessage', 'VehicleRepair:', {255, 0, 0}, "You are not in a car!")
+            end
         end
     end)
 end)
