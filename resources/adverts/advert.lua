2cf08079a22597444e12f48e7ea6a10d7fc48652
cody196 <cody.wherry@gmail.com>
2016-07-18 22:24:06 -0400

    Update advert.lua

--- resources/adverts/advert.lua
+++ resources/adverts/advert.lua
@@ -2,17 +2,17 @@
 
 RegisterServerEvent (onResourceStart, chatMessage)
 
-local chatMessageEntered (console, message)
+local ChatMessageEntered (console, message)
 
 -- the messages, to add more just type print ("the message") on the next line, leave two spaces
-print ("AD: No Deathmatching")
-print ("AD: No Teleporting")
-print ("AD: Need help? Ask an admin or the owner Cody196")
 
 
 -- sends the message to chat
 function chatMessage (message,  all) 
   RegisterServerEvent(ChatMessageEntered, message)
+  print ("AD: No Deathmatching")
+  print ("AD: No Teleporting")
+  print ("AD: Need help? Ask an admin or the owner Cody196")
     wait(120000)   -- wait time in milliseconds
     if wait then
     repeat chatMessage    -- repeats from beginning over and over in a loop
