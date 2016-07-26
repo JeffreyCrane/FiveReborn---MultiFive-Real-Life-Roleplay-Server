9572f28c24d5c02c6f6d07cc32e21034361c26d7
cody196 <cody.wherry@gmail.com>
2016-07-26 18:24:27 -0400

    Update rollup_server.lua

--- resources/rollwindows/rollup_server.lua
+++ resources/rollwindows/rollup_server.lua
@@ -1,6 +1,6 @@
 function RollUpWindow(vehicle, windowIndex)
-  AddEventHandler('chatCommandEntered', function(fullcommand)
-  if(command[1] == "/rollup") then
-    TriggerClientEvent("RollUpWindow", vehicle, command[2]) then
-  print (.. playerName .. "Rolls up his window.")
+        AddEventHandler('chatCommandEntered', function(fullcommand)
+                if(command[1] == "/rollup") then
+                TriggerClientEvent("RollUpWindow", vehicle, command[2]) and then
+        print (.. playerName .. "Rolls up his window.")
 end
