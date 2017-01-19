b5e1526cab7844277a9be14f6c234a2494078440
cody196 <cody.wherry@gmail.com>
2017-01-19 09:20:56 -0500

    Update me.lua

--- resources/MeCommand/me.lua
+++ resources/MeCommand/me.lua
@@ -1,6 +1,10 @@
+local MeCommand = chatMessage;
+
 AddEventHandler('chatCommandEntered', function(fullcommand)
 RegisterServerEvent('chatMessage')
+        
 
+function MeCommand(command, chatMessageEntered)
 if (command[1] == '/me') then
     TriggerClientEvent('chatMessage', -1, GetPlayerName(source), { r, g, b }, command[2])
 end
