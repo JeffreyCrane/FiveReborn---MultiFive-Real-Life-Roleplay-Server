0fa124ef7c995989cc961c5e01d45150ad13b42e
cody196 <cody.wherry@gmail.com>
2017-01-19 09:50:01 -0500

    Update TruckerJob.lua

--- resources/TruckerJob/TruckerJob.lua
+++ resources/TruckerJob/TruckerJob.lua
@@ -21,7 +21,7 @@ local Alpha2 = 200
 ---------------------------------------
 local Truck = {"HAULER", "PACKER", "PHANTOM"}
 local Trainer = {"TANKER", "TRAILERS", "TRAILERS2", "TRAILERLOGS"}
-local Hashtrainer = GAMEPLAY.GET_HASH_KEY(Trainer[1])
+local Hashtrainer = GetHashKey(Trainer[1])
 local Type = 1
 local TruckingDestination = {{0, 0, 0}}
 local blip = {}
@@ -60,14 +60,14 @@ function GUI.unload()
 end
 
 function GUI.init()
-GUI.time = GAMEPLAY.GET_GAME_TIMER()
+GUI.time = GetGameTimer()
 GUI.loaded = true
 
 	for i, coords in pairs(TruckingCompagny) do
-		blip[i] = UI.ADD_BLIP_FOR_COORD(coords[1],coords[2],coords[3])
-		UI.SET_BLIP_SPRITE(blip[i],67) -- 67 /8 9
-        UI.SET_BLIP_SCALE(blip[i], 0.8)
-		UI.SET_BLIP_AS_SHORT_RANGE(blip[i], true)
+		blip[i] = AddBlipForCoord(coords[1],coords[2],coords[3])
+		SetBlipSprite(blip[i],67) -- 67 /8 9
+        	SetBlipScale(blip[i], 0.8)
+		SetBlipAsShortRange(blip[i], true)
 	end
 	
 GUI.addTitle("Trucking Job",0.425, 0.19, 0.45, 0.07)
@@ -142,36 +142,36 @@ function wait(seconds)
 end
 
 function GUI.spawntruck()
-	local Hashtruck = GAMEPLAY.GET_HASH_KEY(Truck[1])
-	STREAMING.REQUEST_MODEL(Hashtruck)				
-	local spawn = ENTITY.GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(PLAYER.PLAYER_PED_ID(), 8.0, 0.0, 0.0)
-	local truck = VEHICLE.CREATE_VEHICLE(Hashtruck, spawn.x, spawn.y, spawn.z, 170, false, false)
-	VEHICLE.SET_VEHICLE_ON_GROUND_PROPERLY(truck)
-	PED.SET_PED_INTO_VEHICLE(PLAYER.PLAYER_PED_ID(), truck, -1);
+	local Hashtruck = GetHashKey(Truck[1])
+	RequestModel(Hashtruck)				
+	local spawn = GetOffsetFromEntityInWorldCoords(PLAYER.PLAYER_PED_ID(), 8.0, 0.0, 0.0)
+	local truck = CreateVehicle(Hashtruck, spawn.x, spawn.y, spawn.z, 170, false, false)
+	SetVehicleOnGroundProperly(truck)
+	SetPedIntoVehicle(PlayerPedId(), truck, -1);
 	print("spawn truck")		
 	wait(1)
 end
 
 function GUI.spawntrainer()
-		STREAMING.REQUEST_MODEL(Hashtrainer)
-		local trainer = VEHICLE.CREATE_VEHICLE(Hashtrainer,602.2297, 266.0714, 103.46, 0, false, false)
-		VEHICLE.SET_VEHICLE_ON_GROUND_PROPERLY(trainer)		
+		RequestModel(Hashtrainer)
+		local trainer = CreateVehicle(Hashtrainer,602.2297, 266.0714, 103.46, 0, false, false)
+		SetVehicleOnGroundProperly(trainer)		
 		print("spawn trainer")
 		wait(1)
 end
 
 function GUI.Money()
-local playerPed = PLAYER.GetPlayerName()
+local playerPed = GetPlayerName()
 
-		if (PED.IS_PED_MODEL(playerPed, GAMEPLAY.GET_HASH_KEY("s_m_m_trucker_01"))) then
+		if (IsPedModel(playerPed, GetHashKey("s_m_m_trucker_01"))) then
 			model = 0x59511A6C
 		end
 	
 		local statname = "SP"..model.."_TOTAL_CASH"
-		local hash = GAMEPLAY.GET_HASH_KEY(statname)
-		local bool, val = STATS.STAT_GET_INT(hash, 0, -1)
+		local hash = GetHashKey(statname)
+		local bool, val = StatGetInt(hash, 0, -1)
 
-		STATS.STAT_SET_INT(hash, val + givemoney, true)
+		StatSetInt(hash, val + givemoney, true)
 
 		money = false
 		count = 1
@@ -180,22 +180,22 @@ end
 function GUI.Marker()
 count = 0
 	for i, coords in pairs(TruckingTrainer) do
-		blip2[i] = UI.ADD_BLIP_FOR_COORD(coords[1],coords[2],coords[3])
-		UI.SET_BLIP_SPRITE(blip2[i],9)
-		UI.SET_BLIP_SCALE(blip2[i], 0.1)
-		UI.SET_BLIP_COLOUR(blip2[i],3)
-		UI.SET_BLIP_AS_SHORT_RANGE(blip2[i], false)	
-		UI.SET_BLIP_ROUTE(blip2[i], true)
+		blip2[i] = AddBlipForCoord(coords[1],coords[2],coords[3])
+		SetBlipSprite(blip2[i],9)
+		SetBlipScale(blip2[i], 0.1)
+		SetBlipColour(blip2[i],3)
+		SetBlipAsShortRange(blip2[i], false)	
+		SetBlipRoute(blip2[i], true)
 	end
 		print("marker trainer")
 
 	for i, coords in pairs(TruckingDestination) do
-		blip3[i] = UI.ADD_BLIP_FOR_COORD(MissionX,MissionY,MissionZ)
-		UI.SET_BLIP_SPRITE(blip3[i],2)
-		UI.SET_BLIP_SCALE(blip3[i], 1.2)
-		UI.SET_BLIP_COLOUR(blip3[i],2)
-		UI.SET_BLIP_AS_SHORT_RANGE(blip3[i], false)
-		UI.SET_BLIP_ROUTE(blip3[i], false)	
+		blip3[i] = AddBlipForCoord(MissionX,MissionY,MissionZ)
+		SetBlipSprite(blip3[i],2)
+		SetBlipScale(blip3[i], 1.2)
+		SetBlipColour(blip3[i],2)
+		SetBlipAsShortRange(blip3[i], false)
+		SetBlipRoute(blip3[i], false)	
 	end							
 		print("marker destination")
 		wait(1)	
@@ -300,21 +300,21 @@ end
 -- Master Function
 
 function GUI.tick()
-local playerPed = PLAYER.PLAYER_PED_ID()
-local player = PLAYER.GET_PLAYER_PED(playerPed)
-local playerExists = ENTITY.DOES_ENTITY_EXIST(playerPed)
-local coords = ENTITY.GET_ENTITY_COORDS( playerPed, nil )
+local playerPed = PlayerPedId()
+local player = GetPlayerPed(playerPed)
+local playerExists = DoesEntityExist(playerPed)
+local coords = GetEntityCoords( playerPed, nil )
 
 	if( playerExists ) then
 				
-				if( GAMEPLAY.GET_DISTANCE_BETWEEN_COORDS( coords.x, coords.y, coords.z,338, 345, 36, false ) < 3 ) then
+				if( GetDistanceBetweenCoords( coords.x, coords.y, coords.z,338, 345, 36, false ) < 3 ) then
 				GUI.drawText()
 				GUI.drawText2() 
-					if(get_key_pressed(111)) then
+					if(GetKeyPressed(111)) then
 						GUI.hiddenMenu1 = false						
 					end
 						
-					if(get_key_pressed(106)) then
+					if(GetKeyPressed(106)) then
 						GUI.spawntruck()
 					end
 				else
@@ -322,19 +322,19 @@ local coords = ENTITY.GET_ENTITY_COORDS( playerPed, nil )
 				GUI.hiddenMenu2 = true	
 				end
 				
-				if( GAMEPLAY.GET_DISTANCE_BETWEEN_COORDS( coords.x, coords.y, coords.z,602.9446, 275.0546, 104.37, false ) < 3 and remover == true) then
+				if( GetDistanceBetweenCoords( coords.x, coords.y, coords.z,602.9446, 275.0546, 104.37, false ) < 3 and remover == true) then
 					for i, coords in pairs(TruckingTrainer) do
-						UI.REMOVE_BLIP(blip2[i])
+						RemoveBlip(blip2[i])
 					end
 
 					for i, coords in pairs(TruckingDestination) do
-						UI.SET_BLIP_ROUTE(blip3[i], true)
+						SetBlipRoute(blip3[i], true)
 					end
 				end
 
-					if( GAMEPLAY.GET_DISTANCE_BETWEEN_COORDS( coords.x, coords.y, coords.z,MissionX , MissionY , MissionZ, false ) < 3 and remover == true) then
+					if( GetDistanceBetweenCoords( coords.x, coords.y, coords.z,MissionX , MissionY , MissionZ, false ) < 3 and remover == true) then
 							for i, coords in pairs(TruckingDestination) do
-									UI.REMOVE_BLIP(blip3[i])
+									RemoveBlip(blip3[i])
 									money = true
 									if (money == true and count == 0) then
 											GUI.Money()
@@ -345,26 +345,26 @@ local coords = ENTITY.GET_ENTITY_COORDS( playerPed, nil )
 					end
 		
 		if (Type == 1) then
-			Hashtrainer = GAMEPLAY.GET_HASH_KEY(Trainer[1])
+			Hashtrainer = GetHashKey(Trainer[1])
 		end
 
 		if (Type == 2) then
-			Hashtrainer = GAMEPLAY.GET_HASH_KEY(Trainer[2])
+			Hashtrainer = GetHashKey(Trainer[2])
 		end
 
 		if (Type == 3) then
-			Hashtrainer = GAMEPLAY.GET_HASH_KEY(Trainer[3])
+			Hashtrainer = GetHashKey(Trainer[3])
 		end
 
 		if (Type == 4) then
-			Hashtrainer = GAMEPLAY.GET_HASH_KEY(Trainer[4])
+			Hashtrainer = GetHashKey(Trainer[4])
 		end
 					
 		if(not GUI.hiddenMenu1)then
 			if( GUI.time == 0) then
-				GUI.time = GAMEPLAY.GET_GAME_TIMER()
+				GUI.time = GetGameTimer()
 			end
-			if((GAMEPLAY.GET_GAME_TIMER() - GUI.time)> 100) then
+			if((GetGameTimer() - GUI.time)> 100) then
 				GUI.updateSelectionMenu1()
 			end	
 			GUI.renderMenu1GUI()	
@@ -375,9 +375,9 @@ local coords = ENTITY.GET_ENTITY_COORDS( playerPed, nil )
 		
 		if(not GUI.hiddenMenu2)then
 			if( GUI.time == 0) then
-				GUI.time = GAMEPLAY.GET_GAME_TIMER()
+				GUI.time = GetGameTimer()
 			end
-			if((GAMEPLAY.GET_GAME_TIMER() - GUI.time)> 100) then
+			if((GetGameTimer() - GUI.time)> 100) then
 				GUI.updateSelectionMenu2()
 			end	
 			GUI.renderMenu2GUI()	
@@ -391,47 +391,47 @@ end
 -- GUI Function 
 
 function GUI.drawText()
-        UI.SET_TEXT_FONT(0)
-        UI.SET_TEXT_PROPORTIONAL(1)
-        UI.SET_TEXT_SCALE(0.0, 0.35)
-        UI.SET_TEXT_COLOUR(255, 255, 255, 255)
-        UI.SET_TEXT_DROPSHADOW(0, 0, 0, 0,255)
-        UI.SET_TEXT_EDGE(1, 0, 0, 0, 255)
-        UI.SET_TEXT_DROP_SHADOW()
-        UI.SET_TEXT_OUTLINE()
-        UI._SET_TEXT_ENTRY("STRING")
-        UI._ADD_TEXT_COMPONENT_STRING("Press NumPad/ to show menu")
-        UI._DRAW_TEXT(0.015, 0.015)
-		GRAPHICS.DRAW_RECT(0.1,0.045,0.18,0.07,0,0,0,200)
+        SetTextFont(0)
+        SetTextProportional(1)
+        SetTextScale(0.0, 0.35)
+        SetTextColour(255, 255, 255, 255)
+        SetTextDropshadow(0, 0, 0, 0,255)
+        SetTextEdge(1, 0, 0, 0, 255)
+        SetTextDropShadow()
+        SetTextOutline()
+        SetTextEntry("STRING")
+        AddTextComponentString("Press NumPad/ to show menu")
+        DrawText(0.015, 0.015)
+		DrawRect(0.1,0.045,0.18,0.07,0,0,0,200)
 end
 
 function GUI.drawText2()
-        UI.SET_TEXT_FONT(0)
-        UI.SET_TEXT_PROPORTIONAL(1)
-        UI.SET_TEXT_SCALE(0.0, 0.35)
-        UI.SET_TEXT_COLOUR(255, 255, 255, 255)
-        UI.SET_TEXT_DROPSHADOW(0, 0, 0, 0,255)
-        UI.SET_TEXT_EDGE(1, 0, 0, 0, 255)
-        UI.SET_TEXT_DROP_SHADOW()
-        UI.SET_TEXT_OUTLINE()
-        UI._SET_TEXT_ENTRY("STRING")
-        UI._ADD_TEXT_COMPONENT_STRING("Press NumPad* to spawn truck")
-        UI._DRAW_TEXT(0.015, 0.045)
+        SetTextFont(0)
+        SetTextProportional(1)
+        SetTextScale(0.0, 0.35)
+        SetTextColour(255, 255, 255, 255)
+        SetTextDropshadow(0, 0, 0, 0,255)
+        SetTextEdge(1, 0, 0, 0, 255)
+        SetTextDropshadow()
+        SetTextOutline()
+        SetTextEntry("STRING")
+        AddTextComponentString("Press NumPad* to spawn truck")
+        DrawText(0.015, 0.045)
 end
 
 
 function GUI.updateSelectionMenu1() 
-	if(get_key_pressed(Keys.NumPad2)) then 
+	if(GetKeyPressed(Keys.NumPad2)) then 
 		if(GUI.selectionmenu1 < GUI.Menu1buttonCount -1  )then
 			GUI.selectionmenu1 = GUI.selectionmenu1 +1
 			GUI.time = 0
 		end
-	elseif (get_key_pressed(Keys.NumPad8) )then
+	elseif (GetKeyPressed(Keys.NumPad8) )then
 		if(GUI.selectionmenu1 > 0)then
 			GUI.selectionmenu1 = GUI.selectionmenu1 -1
 			GUI.time = 0
 		end
-	elseif (get_key_pressed(Keys.NumPad5)) then
+	elseif (GetKeyPressed(Keys.NumPad5)) then
 		if(type(GUI.menu1[GUI.selectionmenu1 +1]["func"]) == "function") then
 			GUI.menu1[GUI.selectionmenu1 +1]["func"](GUI.menu1[GUI.selectionmenu1 +1]["args"])
 		else
@@ -450,17 +450,17 @@ function GUI.updateSelectionMenu1()
 end
 
 function GUI.updateSelectionMenu2() 
-	if(get_key_pressed(Keys.NumPad2)) then 
+	if(GetKeyPressed(Keys.NumPad2)) then 
 		if(GUI.selectionmenu2 < GUI.Menu2buttonCount -1  )then
 			GUI.selectionmenu2 = GUI.selectionmenu2 +1
 			GUI.time = 0
 		end
-	elseif (get_key_pressed(Keys.NumPad8) )then
+	elseif (GetKeyPressed(Keys.NumPad8) )then
 		if(GUI.selectionmenu2 > 0)then
 			GUI.selectionmenu2 = GUI.selectionmenu2 -1
 			GUI.time = 0
 		end
-	elseif (get_key_pressed(Keys.NumPad5)) then
+	elseif (GetKeyPressed(Keys.NumPad5)) then
 		if(type(GUI.menu2[GUI.selectionmenu2 +1]["func"]) == "function") then
 			GUI.menu2[GUI.selectionmenu2 +1]["func"](GUI.menu2[GUI.selectionmenu2 +1]["args"])
 		else
@@ -491,25 +491,25 @@ function GUI.renderMenu2GUI()
 end
 
 function GUI.renderBox(xpos,ypos,xscale,yscale,color1,color2,color3,color4)
-	GRAPHICS.DRAW_RECT(xpos,ypos,xscale,yscale, color1, color2, color3, color4);
+	DrawRect(xpos,ypos,xscale,yscale, color1, color2, color3, color4);
 end
 
 function GUI.renderTitle()
 		for id, settings in pairs(GUI.title) do
 		local screen_w = 0
 		local screen_h = 0
-		screen_w, screen_h =  GRAPHICS.GET_SCREEN_RESOLUTION(0, 0)
+		screen_w, screen_h =  GetScreenResolution(0, 0)
 		boxColor = {0,0,0,255}
-		UI.SET_TEXT_FONT(0)
-		UI.SET_TEXT_SCALE(0.0, 0.40)
-		UI.SET_TEXT_COLOUR(255, 255, 255, 255)
-		UI.SET_TEXT_CENTRE(true)
-		UI.SET_TEXT_DROPSHADOW(0, 0, 0, 0, 0)
-		UI.SET_TEXT_EDGE(0, 0, 0, 0, 0)
-		UI._SET_TEXT_ENTRY("STRING")
-		UI._ADD_TEXT_COMPONENT_STRING(settings["name"])
-		UI._DRAW_TEXT((settings["xpos"] + 0.001), (settings["ypos"] - 0.015))
-		UI._ADD_TEXT_COMPONENT_STRING(settings["name"])
+		SetTextFont(0)
+		SetTextScale(0.0, 0.40)
+		SetTextColour(255, 255, 255, 255)
+		SetTextCentre(true)
+		SetTextDropshadow(0, 0, 0, 0, 0)
+		SetTextEdge(0, 0, 0, 0, 0)
+		SetTextEntry("STRING")
+		AddTextComponentString(settings["name"])
+		DrawText((settings["xpos"] + 0.001), (settings["ypos"] - 0.015))
+		AddTextComponentString(settings["name"])
 		GUI.renderBox(settings["xpos"],settings["ypos"],settings["xscale"],settings["yscale"],boxColor[1],boxColor[2],boxColor[3],boxColor[4])
 		end
 end
@@ -519,21 +519,21 @@ function GUI.renderButtonsMenu1()
 	for id, settings in pairs(GUI.menu1) do
 		local screen_w = 0
 		local screen_h = 0
-		screen_w, screen_h =  GRAPHICS.GET_SCREEN_RESOLUTION(0, 0)
+		screen_w, screen_h =  GetScreenResolution(0, 0)
 		boxColor = {0,0,0,100}
 		if(settings["active"]) then
 			boxColor = {R1,V1,B1,Alpha1}
 		end
-		UI.SET_TEXT_FONT(0)
-		UI.SET_TEXT_SCALE(0.0, 0.35)
-		UI.SET_TEXT_COLOUR(255, 255, 255, 255)
-		UI.SET_TEXT_CENTRE(true)
-		UI.SET_TEXT_DROPSHADOW(0, 0, 0, 0, 0)
-		UI.SET_TEXT_EDGE(0, 0, 0, 0, 0)
-		UI._SET_TEXT_ENTRY("STRING")
-		UI._ADD_TEXT_COMPONENT_STRING(settings["name"])
-		UI._DRAW_TEXT((settings["xpos"] + 0.001), (settings["ypos"] - 0.015))
-		UI._ADD_TEXT_COMPONENT_STRING(settings["name"])
+		SetTextFont(0)
+		SetTextScale(0.0, 0.35)
+		SetTextColour(255, 255, 255, 255)
+		SetTextCentre(true)
+		SetTextDropShadow(0, 0, 0, 0, 0)
+		SetTextEdge(0, 0, 0, 0, 0)
+		SetTextEntry("STRING")
+		AddTextComponentString(settings["name"])
+		DrawText((settings["xpos"] + 0.001), (settings["ypos"] - 0.015))
+		AddTextComponentString(settings["name"])
 		GUI.renderBox(settings["xpos"] ,settings["ypos"], settings["xscale"], settings["yscale"],boxColor[1],boxColor[2],boxColor[3],boxColor[4])
 	 end     
 end
@@ -543,21 +543,21 @@ function GUI.renderButtonsMenu2()
 	for id, settings in pairs(GUI.menu2) do
 		local screen_w = 0
 		local screen_h = 0
-		screen_w, screen_h =  GRAPHICS.GET_SCREEN_RESOLUTION(0, 0)
+		screen_w, screen_h =  GetScreenResolution(0, 0)
 		boxColor = {0,0,0,100}
 		if(settings["active"]) then
 			boxColor = {R2,V2,B2,Alpha2}
 		end
-		UI.SET_TEXT_FONT(0)
-		UI.SET_TEXT_SCALE(0.0, 0.35)
-		UI.SET_TEXT_COLOUR(255, 255, 255, 255)
-		UI.SET_TEXT_CENTRE(true)
-		UI.SET_TEXT_DROPSHADOW(0, 0, 0, 0, 0)
-		UI.SET_TEXT_EDGE(0, 0, 0, 0, 0)
-		UI._SET_TEXT_ENTRY("STRING")
-		UI._ADD_TEXT_COMPONENT_STRING(settings["name"])
-		UI._DRAW_TEXT((settings["xpos"] + 0.001), (settings["ypos"] - 0.015))
-		UI._ADD_TEXT_COMPONENT_STRING(settings["name"])
+		SetTextFont(0)
+		SetTextScale(0.0, 0.35)
+		SetTextColour(255, 255, 255, 255)
+		SetTextCentre(true)
+		SetTextDropShadow(0, 0, 0, 0, 0)
+		SetTextEdge(0, 0, 0, 0, 0)
+		SetTextEntry("STRING")
+		AddTextComponentString(settings["name"])
+		DrawText((settings["xpos"] + 0.001), (settings["ypos"] - 0.015))
+		AddTextComponentString(settings["name"])
 		GUI.renderBox(settings["xpos"] ,settings["ypos"], settings["xscale"], settings["yscale"],boxColor[1],boxColor[2],boxColor[3],boxColor[4])
 	 end     
 end
@@ -566,17 +566,17 @@ function GUI.renderDescription()
 		for id, settings in pairs(GUI.description) do
 		local screen_w = 0
 		local screen_h = 0
-		screen_w, screen_h =  GRAPHICS.GET_SCREEN_RESOLUTION(0, 0)
+		screen_w, screen_h =  GetScreenResolution(0, 0)
 		boxColor = {0,0,0,240}
-		UI.SET_TEXT_FONT(0)
-		UI.SET_TEXT_SCALE(0.0, 0.37)
-		UI.SET_TEXT_COLOUR(255, 255, 255, 255)
-		UI.SET_TEXT_DROPSHADOW(0, 0, 0, 0, 0)
-		UI.SET_TEXT_EDGE(0, 0, 0, 0, 0)
-		UI._SET_TEXT_ENTRY("STRING")
-		UI._ADD_TEXT_COMPONENT_STRING(settings["name"] .. "\n" .."\n" .."Num8 for UP" .. "\n" .. "Num2 for DOWN" .. "\n" .. "Num5 to Select".. "\n" .."Hold H to Detach" .. "\n" .. "Trainer")
-		UI._DRAW_TEXT((settings["xpos"] - 0.06), (settings["ypos"] - 0.13))
-		UI._ADD_TEXT_COMPONENT_STRING(settings["name"])
+		SetTextFont(0)
+		SetTextScale(0.0, 0.37)
+		SetTextColour(255, 255, 255, 255)
+		SetTextDropShadow(0, 0, 0, 0, 0)
+		SetTextEdge(0, 0, 0, 0, 0)
+		SetTextEntry("STRING")
+		AddTextComponentString(settings["name"] .. "\n" .."\n" .."Num8 for UP" .. "\n" .. "Num2 for DOWN" .. "\n" .. "Num5 to Select".. "\n" .."Hold H to Detach" .. "\n" .. "Trainer")
+		DrawText((settings["xpos"] - 0.06), (settings["ypos"] - 0.13))
+		AddTextComponentString(settings["name"])
 		GUI.renderBox(settings["xpos"],settings["ypos"],settings["xscale"],settings["yscale"],boxColor[1],boxColor[2],boxColor[3],boxColor[4])
 		end
 end
