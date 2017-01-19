419f22f55a152bd5a9afd97c155c26092b5c2f4b
cody196 <cody.wherry@gmail.com>
2017-01-19 12:20:23 -0500

    Update rolldown_server.lua

--- resources/rollwindows/rolldown_server.lua
+++ resources/rollwindows/rolldown_server.lua
@@ -1,9 +1,9 @@
 --Roll Down Window Script by Cody196--
 
 function RollDownWindow(vehicle, windowIndex)
-        AddEventHandler('chatCommandEntered', function(fullcommand)
+        AddEventHandler('chatMessage', function(fullcommand)
                 if(command[1] == "/rolldown") then
-                TriggerClientEvent("RollDownWindow", vehicle, command[2]) and then
-        print(.. playerName .. "Rolls down his window.")
+                TriggerClientEvent('RollDownWindow', vehicle, command[2]) and then
+        TriggerClientEvent('chatMessage', .. playerName .. "Rolls down his window.")
         else cancelEvent()
 end
